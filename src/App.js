0c20a18030ad9cb9713bf0a5606d9397fda99281
Mohan <telecomst7@wichita.edu>
2024-06-05 10:00:45 -0500

    clean up + work + push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for the trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="items..."></input>
+      <button>ADD</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return <div className="actions">hi</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You are all set to travel: Logic and condition TBD</em>
+    </footer>
+  );
+}
