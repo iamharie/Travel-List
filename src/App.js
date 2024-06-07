99652c126bf447e157d9bfc3d5f8484023cab2df
Mohan <telecomst7@wichita.edu>
2024-06-07 14:41:25 -0500

    stats: percentage, early return and conditional rendering

--- src/App.js
+++ src/App.js
@@ -125,13 +125,22 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className="stats">
       <em>
-        Total: {numItems} {numPacked}
+        {percentage === 100
+          ? "You got everything ready to go!"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} [${percentage}%]!`}
       </em>
     </footer>
   );
